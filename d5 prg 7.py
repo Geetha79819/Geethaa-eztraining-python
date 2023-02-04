5595656fd79306c10f557968f2ad2de6f9bfb0a6
Geetha79819 <113759845+Geetha79819@users.noreply.github.com>
2023-02-04 09:04:26 +0530

    Add files via upload

---
+++ d5 prg 7.py
@@ -0,0 +1,9 @@
+#without argument,without return value
+def lemons():
+    n1=int(input("enter 1: "))
+    n2=int(input("enter 2: "))
+    n3=int(input("enter 3: "))
+    sum=n1+n2+n3
+    print(sum)
+
+lemons()
