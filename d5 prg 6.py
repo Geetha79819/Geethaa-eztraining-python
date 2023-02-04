4d0addc5f46206d33fa40d0e9739af24d3dbc97a
Geetha79819 <113759845+Geetha79819@users.noreply.github.com>
2023-02-04 09:10:55 +0530

    Add files via upload

---
+++ d5 prg 6.py
@@ -0,0 +1,21 @@
+#with argument,with return value
+#static input
+def multi(n1,n2,n3):
+    prod=n1*n2*n3
+    return prod
+
+
+res=multi(3,4,5)
+print(res)
+
+multi(3,4,5)
+def multi1(a,b,c):
+    prod=a*b*c
+    return prod
+
+n1=int(input("enter 1: "))
+n2=int(input("enter 2: "))
+n3=int(input("enter 3: "))
+res1=multi1(n1,n2,n3)
+print(res1)
+
