e9f35c533138572237dc37412647718e65734e9f
Geetha79819 <113759845+Geetha79819@users.noreply.github.com>
2023-02-04 09:05:55 +0530

    Delete d4 prg 6.py

--- d4 prg 6.py
+++
@@ -1,7 +0,0 @@
-def check_vow(string, vowels):
-    final=(each for each in string if each in vowels)
-    print(len(final))
-    print(final)
-string=' geetha madhuri '
-vowels=' AaEeIiOoUu '
-check_vow(string, vowels);
