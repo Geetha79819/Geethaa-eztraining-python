0ef5a555aa89fea8191191469e39b0ee0552c75e
Geetha79819 <113759845+Geetha79819@users.noreply.github.com>
2023-02-04 19:51:14 +0530

    Add files via upload

---
+++ aaaaa.py
@@ -0,0 +1,6 @@
+n=int(input("enter a number to check it is divisible 2 and 5 : "))
+if n%2==0 & n%5==0:
+    print("it is divisible by both 2 and 5")
+else:
+    print("not divisible")
+            
