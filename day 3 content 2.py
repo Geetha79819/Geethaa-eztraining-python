9fa31bf736c06cdd8dc4463acc891bdc103f0ee6
Geetha79819 <113759845+Geetha79819@users.noreply.github.com>
2023-02-28 13:08:05 +0530

    Add files via upload

---
+++ day 3 content 2.py
@@ -0,0 +1,22 @@
+crt day 4 content -2
+mutable:
+set
+list
+dictionary
+inmutable:
+int
+float
+string
+
+#after getting a string as input check your string contain spaces or not if yes then count no of spaces in it and find it
+#get one string as input along with one character find out and display whether that particular character is present or not
+#check whether string is palindrome or not
+#create a list with vowels get one string as input count vowel from the string and display result#MATH MODULE
+import math
+print( "CeIL 12.5:",math.ceil(12.500
+print( "FLOOR 12.5:",math.floor(12.5))
+print( "SQRT 345:",math.sqrt(345))
+print( "FACTORIAL 3:",math.factorial(3))
+print( "power 2,3:",math.pow(2,3))
+print( "Remainder 10,3:",math.
+    
