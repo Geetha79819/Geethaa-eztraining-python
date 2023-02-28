87452089efc2f8b850542a1afafbdce36ed91567
Geetha79819 <113759845+Geetha79819@users.noreply.github.com>
2023-02-28 11:33:08 +0530

    Add files via upload

---
+++ ds day4 balanced paranthesis.py
@@ -0,0 +1,31 @@
+#checking balanced paranthesis
+s=input()
+st=[]
+balanced=True
+for char in s:
+    if(char=='{' or char=='[' or char=='('):
+        st.append(char)
+    elif(char=='}'):
+        if(len(st) and st.pop()!='{'):
+            balanced=False
+            break
+    elif(char==']'):
+        if(len(st) and st.pop()!='['):
+            balanced=False
+            break
+    elif(char==')'):
+        if(len(st) and st.pop()!='('):
+            balanced=False
+            break
+            
+    else:
+         balanced=False
+         break
+if(balanced==False or len(st)):
+    print("not balanced")
+else:
+    print("balanced")
+    
+        
+
+    
